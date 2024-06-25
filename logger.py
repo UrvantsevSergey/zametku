d2faffd412afb1cd7eca44699678ac6518364a36
UrvantsevSergey <dbktmn@gmail.com>
2024-06-25 17:43:23 +0500

    Чтение и запись без ид и даты

--- logger.py
+++ logger.py
@@ -7,20 +7,18 @@ import uuid
 def read_file():
     with open('zametki.csv', 'r') as f:
         data = f.read()  
-        text = tk.Text()
+        display = tk.Toplevel()
+        display.title("Заметки")
+        display.geometry("300x200")
+        text = tk.Text(display)
         text.pack()
         text.insert('1.0', data) 
-        f.close()
-        
 
 #Создание заметки
 def create_file():
     def save_file():
         with open('zametki.csv', 'a') as f:
-            #text = f"{r.get()}" + "\n"
-            id = str(uuid.uuid4())
-            dtime = datetime.now()
-            text = f"{id}\n{dtime}\n{insert.get()}\n"
+            text = f"{insert.get()}" + "\n"
             f.write(text)
             window.destroy()
 
@@ -30,7 +28,6 @@ def create_file():
 
     insert = ttk.Entry(window)
     insert.pack()
-
 
     save_button = ttk.Button(window, text="Сохранить в файл", command=save_file)
     save_button.pack()
